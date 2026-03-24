f14b5128cadaf704e9c2fd2c6a83d0e094218641
Yago <pavanyago98@gmail.com>
2026-03-24 11:13:11 -0300

    Final dia 24/03/2026

--- lib/main_widgets.dart
+++ lib/main_widgets.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(Icons.thumb_up, size: 60),
+              Icon(Icons.heart_broken, size: 60),
 
               Text(
                 "D.S. é o melhor curso de SESI?",
