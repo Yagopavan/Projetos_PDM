2a38fbb9c5438afeb62e71e766badf3c38c9cf43
Yago <pavanyago98@gmail.com>
2026-03-31 11:15:46 -0300

    Final dia 31/03/2026

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,61 +9,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: ContadorTela());
-  }
-}
-
-class ContadorTela extends StatefulWidget {
-  const ContadorTela({super.key});
-
-  @override
-  _ContadorTelaState createState() => _ContadorTelaState();
-}
-
-class _ContadorTelaState extends State<ContadorTela> {
-  int contador = 0;
-
-  void incrementar() {
-    setState(() {
-      contador++;
-    });
-  }
-
-  void desincrementar() {
-    setState(() {
-      if (contador > 0) {
-        contador--;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('minha tela')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.favorite, size: 60, color: Colors.red),
-            Text("Curtidas: $contador", style: TextStyle(fontSize: 22)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: incrementar,
-                  child: Text("👍 Gostei"),
-                ),
-                SizedBox(width: 16),
-                ElevatedButton(
-                  onPressed: desincrementar,
-                  child: Text("👎Não Gostei"),
-                ),
-              ],
-            ),
-          ],
+    return  MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Texto Imagem"),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Bem Vindo",
+                style: TextStyle(fontSize: 24),
+              ),
+              const SizedBox(height: 20),
+              Image.asset(
+                'assets/images/bmw_m3.jpg',
+                width: 200,
+              )
+            ],
+          ),
+          ),
+        ),
+      );
   }
 }
